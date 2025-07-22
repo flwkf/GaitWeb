fb5f5fbbb1f6f7f298b9fdc808bb0215c3bbefeb
Rizal Rahman Rizkika <94729829+flwkf@users.noreply.github.com>
2025-07-22 08:36:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 import streamlit as st
-st.markdown("""
-    <style>
-    html, body, [data-testid="stAppViewContainer"] {
-        zoom: 0.95 !important;
-    }
-    </style>
-""", unsafe_allow_html=True)
 
 pages = {
     "Menu": [
