47d2dd065086a481ab7c96042ebc2e7eb3ff4f31
Rizal Rahman Rizkika <94729829+flwkf@users.noreply.github.com>
2025-07-22 08:28:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import streamlit as st
-zoom_level = 0.75
-st.markdown(f"""
-    <style>
-        .main {{
-            transform: scale({zoom_level});
-            transform-origin: top left;
-            width: {100 / zoom_level}%;
-        }}
-    </style>
-""", unsafe_allow_html=True)
+
 pages = {
     "Menu": [
         st.Page("dashboard.py", title="Dashboard"),
